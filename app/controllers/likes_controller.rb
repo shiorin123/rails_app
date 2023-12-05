bea7204b6cb940aff3c7589607c2a82e6cb80c66
shiorin123 <e5621012@u-shimane.ac.jp>
2023-12-05 15:53:28 +0900

    Initial Commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
 
     def create
-        like = current_user.likes.create(good_id: params[:good_id]) 
-        redirect_back(fallback_location: root_path)
-      end
+      like = current_user.likes.create(good_id: params[:good_id]) 
+      redirect_back(fallback_location: root_path)
+    end
     
-      def destroy
-        like = Like.find_by(good_id: params[:good_id], user_id: current_user.id)
-        like.destroy
-        redirect_back(fallback_location: root_path)
+    def destroy
+      like = Like.find_by(good_id: params[:good_id], user_id: current_user.id)
+      like.destroy
+      redirect_back(fallback_location: root_path)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show] 
+
   resources :goods do
     resources :likes, only: [:create, :destroy]
   end
 
   resources :comments, only: [:create]
 
-  resources :users, only: [:show] 
 
   resources :relationships, only: [:create, :destroy]
 
+  
   root 'goods#index'
 
   Rails.application.routes.draw do
